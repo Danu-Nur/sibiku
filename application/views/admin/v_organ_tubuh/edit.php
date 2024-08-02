d7255834273e2b5d93079bb324e94cc37b20ad61
Danu-Nur <diramadhan13@gmail.com>
2024-08-02 22:21:01 +0700

    fix view edit organ tubuh

--- application/views/admin/v_organ_tubuh/edit.php
+++ application/views/admin/v_organ_tubuh/edit.php
@@ -1,11 +1,17 @@
 <div class="row">
-	<div class="col-md-12 col-sm-12 col-xs-12">
-		<div class="x_panel">
-			<div class="x_title" style="display: flex;justify-content: space-between;align-items: center;">
-				<h2>Form <?= $judul ?></h2>
-				<a href="<?= base_url('Admin/OrganTubuh') ?>" type="button" class="btn btn-primary"><i class="fa fa-arrow-left"></i> Kembali</a>
+	<div class="x_panel">
+		<div class="x_title" style="display: flex;justify-content: space-between;align-items: center;">
+			<h2>Form <?= $judul ?></h2>
+			<a href="<?= base_url('Admin/OrganTubuh') ?>" type="button" class="btn btn-primary"><i class="fa fa-arrow-left"></i> Kembali</a>
+		</div>
+		<div class="clearfix"></div>
+		<div class="col-md-3 col-sm-12 col-xs-12">
+
+			<div class="x_content" style="display: flex; justify-content: end;">
+				<img src="<?= base_url('uploads/web/organtubuh.png') ?>" width="250px" alt="">
 			</div>
-			<div class="clearfix"></div>
+		</div>
+		<div class="col-md-9 col-sm-12 col-xs-12">
 			<div class="x_content">
 				<br />
 				<form id="demo-form2" action="<?= site_url('Admin/OrganTubuh/update') ?>" method="POST" enctype="multipart/form-data" data-parsley-validate class="form-horizontal form-label-left">
@@ -31,7 +37,7 @@
 							<input type="file" id="link_video" name="link_video" required="required" class="form-control col-md-7 col-xs-12">
 						</div>
 					</div>
-					<div class="ln_solid"></div>
+					<!-- <div class="ln_solid"></div> -->
 					<div class="form-group">
 						<div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3">
 							<button type="submit" class="btn btn-success">Simpan</button>
